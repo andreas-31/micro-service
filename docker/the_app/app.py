144337f044ff07a91dd46189b2808847228b43de
Andreas Gaupmann <andreas@gaupmann.net>
2020-06-27 18:52:31 +0200

    feat: Dockerized the Flask web app

--- docker/the_app/app.py
+++ docker/the_app/app.py
@@ -1,13 +1,3 @@
-#from flask import Flask 
-#app = Flask(__name__) 
-
-#@app.route('/hello/<name>') 
-#def hello_name(name): 
-#    return 'Hello %s!' % name 
-
-#if __name__ == '__main__': 
-#    app.run(host='0.0.0.0') 
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -17,12 +7,16 @@ def home():
     return render_template("home.html")
 
 @app.route("/andreas")
-def salvador():
+def andreas():
     return "Hello, Andreas"
+
+@app.route('/hello/<name>') 
+def hello_name(name): 
+    return 'Hello %s!' % name
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
